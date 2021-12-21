1a6ce9d44f1d0cdf12ec9c4395e23ec8de27c78e
Vikramaditya Gaonkar <vikram@dataroots.io>
2021-12-22 00:31:31 +0100

    Fix the minimal IAM privilege

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,26 +1,34 @@
 resource "aws_iam_role_policy" "presigned_url_iam_role_policy" {
-  name = "presigned_url_iam_role_policy"
+  name   = "presigned_url_iam_role_policy"
   role   = aws_iam_role.presigned_url_iam_role.id
   policy = jsonencode({
-    Version="2012-10-17",
-    Statement=[
+    Version   = "2012-10-17",
+    Statement = [
       {
-        Effect="Allow",
-        Action=[
+        Effect   = "Allow"
+        Action   = [
           "iot:Publish",
-        ],
-        Resource=["arn:aws:iot:${var.region}:${local.account_id}:topic/*"]
+        ]
+        # Can fix the topic name to what is in the send_large_file.py for extra security
+        Resource = ["arn:aws:iot:${var.region}:${local.account_id}:topic/*"]
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:PutObject",
+        ]
+        Resource = ["arn:aws:s3:::${var.s3_bucket_name}/*"]
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "iam_role_lambda_execution" {
+resource "aws_iam_role_policy_attachment" "policy_att_presigned_url" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.presigned_url_iam_role.name
 }
 resource "aws_iam_role" "presigned_url_iam_role" {
-  name               = "presigned_url_iam_role"
+  name               = "presigned_url"
   assume_role_policy = jsonencode({
     Version   = "2012-10-17"
     Statement = [
@@ -37,21 +45,34 @@ resource "aws_iam_role" "presigned_url_iam_role" {
 }
 
 resource "aws_iam_role_policy" "send_email_iam_role_policy" {
-  name = "send_email_iam_role_policy"
-  role   = aws_iam_role.presigned_url_iam_role.id
+  name   = "send_email_iam_role_policy"
+  role   = aws_iam_role.send_email_iam_role.id
   policy = jsonencode({
-    Version="2012-10-17",
-    Statement=[
+    Version   = "2012-10-17",
+    Statement = [
       {
-        Effect="Allow",
-        Action=[
+        Effect   = "Allow",
+        Action   = [
           "s3:GetObject"
         ],
-        Resource=["*"]
+        Resource = ["arn:aws:s3:::${var.s3_bucket_name}/*"]
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "ses:SendEmail"
+        ],
+        Resource = ["arn:aws:ses:${var.region}:${local.account_id}:identity/*"]
       }
     ]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "policy_att_send_email" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.send_email_iam_role.name
+}
+
 resource "aws_iam_role" "send_email_iam_role" {
   name               = "send_email_iam_role"
   assume_role_policy = jsonencode({
@@ -85,7 +106,7 @@ resource "aws_iot_policy" "pubsub" {
           "iot:Subscribe",
           "iot:Receive",
         ],
-        Resource=[
+        Resource = [
           "arn:aws:iot:${var.region}:${local.account_id}:cacert/*",
           "arn:aws:iot:${var.region}:${local.account_id}:client/*",
           "arn:aws:iot:${var.region}:${local.account_id}:topic*/*",
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,30 +1,30 @@
 resource "aws_lambda_function" "lambda_func" {
   function_name = "generate_pre_signed_s3_url"
-  handler = "publish_presigned_url_lambda.lambda_handler"
+  handler       = "publish_presigned_url_lambda.lambda_handler"
   role          = aws_iam_role.presigned_url_iam_role.arn
-  runtime = "python3.9"
-  filename = data.archive_file.lambda_zip_file.output_path
-  layers = ["arn:aws:lambda:eu-central-1:770693421928:layer:Klayers-python38-aws-xray-sdk:100"]
-  timeout = 5
+  runtime       = "python3.9"
+  filename      = data.archive_file.lambda_zip_file.output_path
+  layers        = ["arn:aws:lambda:${var.region}:770693421928:layer:Klayers-python38-aws-xray-sdk:100"]
+  timeout       = 5
 }
 
 data "archive_file" "lambda_zip_file" {
   output_path = "/tmp/lambda_zip_file_int.zip"
-  type = "zip"
+  type        = "zip"
   source {
-    content = file("./files/publish_presigned_url_lambda.py")
+    content  = file("./files/publish_presigned_url_lambda.py")
     filename = "publish_presigned_url_lambda.py"
   }
 }
 
 resource "aws_lambda_function" "email_lambda" {
   function_name = "email_lambda"
-  handler = "email_lambda.lambda_handler"
+  handler       = "email_lambda.lambda_handler"
   role          = aws_iam_role.send_email_iam_role.arn
-  runtime = "python3.9"
-  filename = data.archive_file.lambda_email_zip.output_path
-  layers = ["arn:aws:lambda:eu-central-1:770693421928:layer:Klayers-python38-aws-xray-sdk:100"]
-  timeout = 5
+  runtime       = "python3.9"
+  filename      = data.archive_file.lambda_email_zip.output_path
+  layers        = ["arn:aws:lambda:${var.region}:770693421928:layer:Klayers-python38-aws-xray-sdk:100"]
+  timeout       = 5
   environment {
     variables = {
       FROM_ADDRESS = var.from_address
@@ -35,9 +35,9 @@ resource "aws_lambda_function" "email_lambda" {
 
 data "archive_file" "lambda_email_zip" {
   output_path = "/tmp/lambda_email.zip"
-  type = "zip"
+  type        = "zip"
   source {
-    content = file("./files/email_lambda.py")
+    content  = file("./files/email_lambda.py")
     filename = "email_lambda.py"
   }
 }
@@ -56,6 +56,6 @@ resource "aws_lambda_permission" "allow_s3_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.email_lambda.function_name
   principal     = "s3.amazonaws.com"
-  source_arn    =aws_s3_bucket.s3_bucket.arn
+  source_arn    = aws_s3_bucket.s3_bucket.arn
 }
 
